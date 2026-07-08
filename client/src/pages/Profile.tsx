4badc572b5151da480c85b089f8c9d0064042eca
Ugarte Casas, Iñaki <inaki.ugarte@altia.es>
2026-07-08 20:34:19 +0200

    Corrección de bug en producción

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import Button from '../components/Button';
