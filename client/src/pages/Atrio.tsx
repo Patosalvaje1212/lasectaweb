367017bfdac368b908a504c21282ed07a1f460d7
Ugarte Casas, Iñaki <inaki.ugarte@altia.es>
2026-07-17 11:27:22 +0200

    Corrección de bug

--- client/src/pages/Atrio.tsx
+++ client/src/pages/Atrio.tsx
@@ -1,5 +1,5 @@
 // client/src/pages/Homepage.tsx
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import CalendarWidgets from '../components/CalendarWidget'
